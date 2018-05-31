07d4b9a41689ed9047c8635054096bab4163a031
Adham Abozaeid <adham.abozaeid@microchip.com>
2018-05-31 16:19:58 -0700

    remove outdated prebuilt images

--- wilc_sama5d4_linux_demo_4.12_kernel/demo_linux_nandflash.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sam-ba /dev/ttyACM0 at91sama5d4x-ek demo_linux_nandflash.tcl > logfile.log 2>&1
-cat logfile.log
--- wilc_sama5d4_linux_demo_kernel_v4.1.0/demo_linux_nandflash.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/usr/bin/sam-ba_cdc_linux/sam-ba /dev/ttyACM0 at91sama5d4x-ek demo_linux_nandflash.tcl 2>&1 | tee logfile.log
